89d5f9a6fe006f8c77a0c53e65fd3f670d1fa73c
Bharti <malibharti5@gmail.com>
2020-11-28 22:48:23 +0530

    ADD : Part time wage calculation is added

--- employeeWage.py
+++ employeeWage.py
@@ -7,16 +7,34 @@ class Employee:
         else:
             return "absent"
 
-    def calculateEmployeeDailyWage(self):
-        attendance = emp.checkAttendance()
-        if attendance == "present":
-            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR
+    def checkPartTime(self):
+        isPartTime=random.randint(0,1)
+        if isPartTime==0:
+            return "true"
         else:
-            dailyWage = 0
+            return "false"
+
+    def calculateEmployeeDailyWage(self):
+        """
+        checks attendance and daily hours
+        calculates daily wage
+        :return dailyWage:
+        """
+        attendance = emp.checkAttendance()
+        partTime = emp.checkPartTime()
+        dailyWage=0
+        if attendance == "present":
+            if partTime == "false":
+                dailyWorkingHour = FULL_DAY_HOUR
+            else:
+                dailyWorkingHour = PART_TIME_HOUR
+            dailyWage = WAGE_PER_HOUR * dailyWorkingHour
+
         return dailyWage
 
 if __name__=="__main__":
     WAGE_PER_HOUR=20
     FULL_DAY_HOUR=8
+    PART_TIME_HOUR=4
     emp=Employee()
     print(emp.calculateEmployeeDailyWage())
