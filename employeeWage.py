051acfde8f4bb88096787af686a6f2602b8b9d29
malibharti5@gmail.com <malibharti5@gmail.com>
2020-11-28 23:56:55 +0530

    Bharti-REFACTOR : DailyWage calculation using switch case statement

--- employeeWage.py
+++ employeeWage.py
@@ -1,19 +1,22 @@
 import random
+
+
 class Employee:
     def checkAttendance(self):
-        number=random.randint(0,1)
-        if number == 1:
-            return "present"
-        else:
-            return "absent"
+        number = random.randint(0, 1)
+        switcher = {
+            0: "absent",
+            1: "present"
+        }
+        return switcher.get(number)
 
     def checkPartTime(self):
-        isPartTime=random.randint(0,1)
-        if isPartTime==0:
-            return "true"
-        else:
-            return "false"
-
+        is_part_time = random.randint(0, 1)
+        switcher = {
+            0: FULL_DAY_HOUR,
+            1: PART_TIME_HOUR
+        }
+        return switcher.get(is_part_time)
     def calculateEmployeeDailyWage(self):
         """
         checks attendance and daily hours
@@ -21,20 +24,16 @@ class Employee:
         :return dailyWage:
         """
         attendance = emp.checkAttendance()
-        partTime = emp.checkPartTime()
+        dailyWorkingHour = emp.checkPartTime()
         dailyWage=0
         if attendance == "present":
-            if partTime == "false":
-                dailyWorkingHour = FULL_DAY_HOUR
-            else:
-                dailyWorkingHour = PART_TIME_HOUR
             dailyWage = WAGE_PER_HOUR * dailyWorkingHour
 
         return dailyWage
 
-if __name__=="__main__":
-    WAGE_PER_HOUR=20
-    FULL_DAY_HOUR=8
-    PART_TIME_HOUR=4
-    emp=Employee()
+if __name__ == "__main__":
+    WAGE_PER_HOUR = 20
+    FULL_DAY_HOUR = 8
+    PART_TIME_HOUR = 4
+    emp = Employee()
     print(emp.calculateEmployeeDailyWage())
