3ee433763ab7f9a2b77c64d68e8574cd28f6d748
malibharti5@gmail.com <malibharti5@gmail.com>
2020-11-29 00:09:23 +0530

    Bharti-ADD : Monthly wage computation is added

--- employeeWage.py
+++ employeeWage.py
@@ -17,6 +17,7 @@ class Employee:
             1: PART_TIME_HOUR
         }
         return switcher.get(is_part_time)
+
     def calculateEmployeeDailyWage(self):
         """
         checks attendance and daily hours
@@ -25,15 +26,24 @@ class Employee:
         """
         attendance = emp.checkAttendance()
         dailyWorkingHour = emp.checkPartTime()
-        dailyWage=0
+        dailyWage = 0
         if attendance == "present":
             dailyWage = WAGE_PER_HOUR * dailyWorkingHour
 
         return dailyWage
+
+    def monthlyWageComputation(self):
+        count = 1
+        monthlyWage=0
+        while count <= 20:
+            tempWage = emp.calculateEmployeeDailyWage()
+            monthlyWage=monthlyWage+tempWage
+            count = count + 1
+        return monthlyWage
 
 if __name__ == "__main__":
     WAGE_PER_HOUR = 20
     FULL_DAY_HOUR = 8
     PART_TIME_HOUR = 4
     emp = Employee()
-    print(emp.calculateEmployeeDailyWage())
+    print(emp.monthlyWageComputation())
