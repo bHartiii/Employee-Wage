3f66746bf2b80b543df0e5258b6ce6cee73ac1da
Bharti <malibharti5@gmail.com>
2020-11-28 21:27:52 +0530

    ADD : Employee daily wage calculation feature is added

--- employeeWage.py
+++ employeeWage.py
@@ -7,6 +7,16 @@ class Employee:
         else:
             return "absent"
 
+    def calculateEmployeeDailyWage(self):
+        attendance = emp.checkAttendance()
+        if attendance == "present":
+            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR
+        else:
+            dailyWage = 0
+        return dailyWage
+
 if __name__=="__main__":
+    WAGE_PER_HOUR=20
+    FULL_DAY_HOUR=8
     emp=Employee()
-    print(emp.checkAttendance())
+    print(emp.calculateEmployeeDailyWage())
