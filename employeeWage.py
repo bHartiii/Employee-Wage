93c6ca4ec0b73277c748d8dec27c04cd386d1f5e
Bharti <malibharti5@gmail.com>
2020-11-28 16:21:26 +0530

    BHARTI ADD : Employee attendance check

--- employeeWage.py
+++ employeeWage.py
@@ -1,1 +1,12 @@
-print("Welcome to employee wage computation program")
+import random
+class Employee:
+    def checkAttendance(self):
+        number=random.randint(0,1)
+        if number == 1:
+            return "present"
+        else:
+            return "absent"
+
+if __name__=="__main__":
+    emp=Employee()
+    print(emp.checkAttendance())
